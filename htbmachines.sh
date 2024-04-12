e805c846604714cd683e56f205491e93b9b96ca0
matigam <4,.m.0.rfe.er0>
2024-04-12 05:38:10 -0400

    test

---
+++ htbmachines.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+function ctrl_c(){
+ echo -e "\n\n${redColour}[!] Saliendo...${endColour}\n"
+ tput cnorm && exit 1
+}
+
+# Ctrl+C 
+trap ctrl_c INT
+#read
+
+# Variables globales
+main_url="https://htbmachines.github.io/bundle.js"
+
+
+function helpPanel(){
+  echo -e "\n${yellowColour}[+]${endColour}${grayColour} Uso:${endColour}"
+  echo -e "\t${purpleColour}u)${endColour}${grayColour} Descargar o actualizar archivos necesarios${endColour}"
+  echo -e "\t${purpleColour}m)${endColour}${grayColour} Buscar por nombre de maquina${endColour}"
+  echo -e "\t${purpleColour}i)${endColour}${grayColour} Buscar por direccion IP${endColour}"
+  echo -e "\t${purpleColour}h)${endColour}${grayColour} Mostrar este panel de ayuda${endColour}" 
+}
+
+function updateFiles ()
+{
+  if [ ! -f bundle.js ]; then
+    tput civis
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Descargando archivos necesarios...${endColour}"
+    curl -s $main_url > bundle.js
+    js-beautify bundle.js | sponge bundle.js
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Todos los archivos han sido descargados.${endColour}"
+    tput cnorm
+  else 
+    tput civis
+    echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Comprobando si hay actualizaciones pendientes...${endColour}"
+    curl -s $main_url > bundle_temp.js 
+    js-beautify bundle_temp.js | sponge bundle_temp.js 
+    md5_temp_value=$(md5sum bundle_temp.js | awk '{print $1}')
+    md5_original_value=$(md5sum bundle.js | awk '{print $1}')
+
+    if [ "$md5_temp_value" == "$md5_original_value" ]; then
+      echo -e "\n${yellowColour}[+] ${endColour}${grayColour}No se han encontrado actualizaciones${endColour}"
+      rm bundle_temp.js 
+    else
+      echo -e "\n${yellowColour}[+]${endColour}${grayColour} Se han encontrado actualizaciones disponibles${endColour}"
+      sleep 1
+      rm bundle.js && mv bundle_temp.js bundle.js 
+
+      echo -e "\n${yellowColour}[+]${endColour}${grayColour} Los archivos han sido actualizados${endColour}"
+    fi
+    tput cnorm
+  fi
+}
+
+function searchMachine(){
+  machineName="$1"
+
+  echo -e "\n${yellowColour}[+]${endColour}${grayColour} Listando las propiedades de la maquina${endColour}${blueColour} $machineName${endColour}\n"
+
+  cat bundle.js | awk "/name: \"$machineName\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta" | tr -d '"' | tr -d ',' | sed 's/^ *//'
+}
+ 
+function searchIP ()
+{
+  ipAddress="$1"
+  machineName="$(cat bundle.js | grep "ip: \"$ipAddress\"" -B 3 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',')"
+  echo -e "\n${yellowColour}[+]${endColour} ${grayColour}La maquina correspondiente para la IP${endColour}${blueColour} $ipAddress${endColour}${grayColour} es${endColour}${redColour} $machineName${endColour}\n"
+}
+
+#Indicadores
+declare -i parameter_counter=0
+
+while getopts "m:ui:h" arg; do 
+  case $arg in 
+    m) machineName=$OPTARG; let parameter_counter+=1;;
+    u) let parameter_counter+=2;;
+    i) ipAddress=$OPTARG; let parameter_counter+=3;;
+    h) ;;
+  esac
+done
+
+if [ $parameter_counter -eq 1 ]; then
+  searchMachine $machineName
+elif [ $parameter_counter -eq 2 ]; then
+  updateFiles
+elif [ $parameter_counter -eq 3 ]; then
+  searchIP $ipAddress
+else
+  helpPanel
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
